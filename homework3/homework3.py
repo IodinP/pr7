9c30807705bf84c02f9c4932e4bd3083351d3be0
IodinP <168574718+IodinP@users.noreply.github.com>
2025-02-17 17:43:32 +0500

    Добавлены задания homework2 и homework3

---
+++ homework3/homework3.py
@@ -0,0 +1,8 @@
+# вариант 3:пятеричная
+def solve_equation(a, b, c):
+    return a + b * 4 - c
+a = float(input("Введите значение для a: "))
+b = float(input("Введите значение для b: "))
+c = float(input("Введите значение для c: "))
+result = solve_equation(a, b, c)
+print(f"Результат уравнения x = a + b * 4 - c: {result}")
